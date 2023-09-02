95bdfa87dc349a4d8e0921758bc7a6264e16a1f2
Caleb Ho <calebh@meta.com>
2023-09-01 18:38:51 -0700

    Fix exe check

--- src/stub_launcher.cc
+++ src/stub_launcher.cc
@@ -261,7 +261,7 @@ StubLauncher::Launch()
   // If running stub process without any arguments returns any status code,
   // other than 1, it can indicate a permission issue as a result of
   // downloading the stub process from a cloud object storage service.
-  if (WEXITSTATUS(stub_status_code) != 1) {
+  if (WEXITSTATUS(stub_status_code) == 126) {
     // Give the execute permission for the triton_python_backend_stub to the
     // owner.
     int error = chmod(python_backend_stub.c_str(), S_IXUSR);
